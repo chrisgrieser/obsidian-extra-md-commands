28c045537fe5fb46f39c3b3dfe3b00047b2da4b8
pseudometa <73286100+chrisgrieser@users.noreply.github.com>
2021-11-04 22:35:38 +0100

    underscore bold and italics

--- main.ts
+++ main.ts
@@ -1,137 +1,86 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import { MarkdownView, Plugin, EditorPosition } from "obsidian";
 
-// Remember to rename these classes and interfaces!
+export default class AlternativeMDSyntax extends Plugin {
+  async onload() {
+    this.addCommand({
+      id: "underscore-bold",
+      name: "Underscore Bold",
+      callback: () => this.wrapSelection("__", "__"),
+      hotkeys: [
+        {
+          modifiers: ["Mod","Shift"],
+          key: "b",
+        },
+      ],
+    });
 
-interface MyPluginSettings {
-	mySetting: string;
-}
+    this.addCommand({
+      id: "underscore-italics",
+      name: "Underscore Italics",
+      callback: () => this.wrapSelection("_","_"),
+      hotkeys: [
+        {
+          modifiers: ["Mod","Shift"],
+          key: "i",
+        },
+      ],
+    });
+    console.log ("Alternative Markdown Syntax Plugin loaded.");
+  }
 
-const DEFAULT_SETTINGS: MyPluginSettings = {
-	mySetting: 'default'
-}
 
-export default class MyPlugin extends Plugin {
-	settings: MyPluginSettings;
+  wrapSelection(beforeStr: string, afterStr: string): void {
+    //check whether active note is in editor mode
+    let markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
+    if (!markdownView) return;
+    let editor = markdownView.editor;
 
-	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
+    let selectedText = "";
+    if (editor.somethingSelected()) selectedText = editor.getSelection();
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+    function Cursor(pos: number): EditorPosition {
+      return editor.offsetToPos(pos);
+    }
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+    // Detect whether the selected text is surrounded with __ __
+    // If true, unpack it, else pack with __ __
+    const sp = editor.posToOffset(editor.getCursor("from")); // Starting position
+    const len = selectedText.length;
+    const blen = beforeStr.length;
+    const alen = afterStr.length;
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
-				}
-			}
-		});
+    const charsBefore = editor.getRange(Cursor(sp - blen), Cursor(sp));
+    const charsAfter = editor.getRange(Cursor(sp + len), Cursor(sp + len + alen));
 
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
+    const firstChars = editor.getRange(Cursor(sp), Cursor(sp + blen));
+    const lastChars = editor.getRange(Cursor(sp + len - alen), Cursor(sp + len));
 
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
+   // Undo surrounding Syntax (outside selection)
+    if (charsBefore == beforeStr && charsAfter == afterStr) {
+      editor.setSelection(Cursor(sp - blen), Cursor(sp + len + alen));
+      editor.replaceSelection(selectedText);
+      editor.setSelection(Cursor(sp - blen), Cursor(sp + len - alen));
 
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
+   // Undo surrounding Syntax (inside selection)
+    } else if (firstChars == beforeStr && lastChars == afterStr) {
+      editor.replaceSelection(selectedText.slice(blen,-alen));
+      editor.setSelection(Cursor(sp), Cursor(sp + len - (blen + alen)));
 
-	onunload() {
+   // Wrap Syntax around Selection
+    } else {
+      if (selectedText) {
+        editor.replaceSelection(beforeStr + selectedText + afterStr);
+        editor.setSelection(Cursor(sp + blen), Cursor(sp + len + alen));
+      // No Selection
+      } else {
+        editor.replaceSelection(beforeStr + afterStr);
+        let cursor = editor.getCursor();
+        cursor.ch -= alen;
+        editor.setCursor(cursor);
+      }
+    }
 
-	}
 
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		containerEl.createEl('h2', {text: 'Settings for my awesome plugin.'});
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					console.log('Secret: ' + value);
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
-	}
+  }
 }
